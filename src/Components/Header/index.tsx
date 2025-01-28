d7a3196ba51ad43d2c9ae61abfa68ce13ac52df5
Vladimir <51049529+Goodbrvtb@users.noreply.github.com>
2025-01-28 23:38:04 +0300

    Revert "Feature/add burger menu"

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,17 +1,13 @@
-import { FC, useEffect, useState } from 'react';
+import { FC, useEffect } from 'react';
 import { useLocation, useNavigate } from "react-router-dom";
 import bookmark from "../../assets/logos/bookmark.svg";
 import homeimg from "../../assets/logos/home.svg";
 import logo from "../../assets/logos/museum-logo.svg";
 import { FAVORITE_ROUTE, HOME_ROUTE } from "../../routes";
 import './style.scss';
-import iconBurgerMenu from "../../assets/logos/burgerButton.svg"
-import closeBurgerMenu from "../../assets/logos/closeBurgerButton.svg"
 
 
 export const Header: FC = () => {
-  const [isOpen, setOpen] = useState(false);
-
   const navigate = useNavigate();
   const location = useLocation();
 
@@ -29,11 +25,8 @@ export const Header: FC = () => {
           <img className="logo-img" src={logo} alt="" ></img>
         </div>
 
-        <div className={`fav-and-home-button ${isOpen ? "active" : ""}`} >
-          <button className='close-menu-button' onClick={() => setOpen(!isOpen)}>
-            <img src={closeBurgerMenu} alt="Close burger menu"></img>
-          </button>
-          <div className='navigate-button' >
+        <div className='fav-and-home-button'>
+          <div className='navigate-button'>
 
             {!isHome && (<div className='home'>
               <button onClick={handleHomeClick}>
@@ -47,16 +40,14 @@ export const Header: FC = () => {
                 <img src={bookmark} alt="Favorites"></img>
                 <div className='your-favorites'>Your favorites </div>
               </button>
-
             </div>
-          </div>
 
+
+          </div>
         </div>
-        <button className='header-menu-button' onClick={() => setOpen(!isOpen)}>
-          <img src={iconBurgerMenu} alt="burgerButton"></img>
-        </button>
+
       </div>
-    </header >
+    </header>
   );
 };
 
