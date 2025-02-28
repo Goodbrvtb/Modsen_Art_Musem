4c82126340aae11e210f4d774b791295b6209471
Vova <petrovic_h@mail.ru>
2025-02-28 17:48:10 +0300

    refactoring paginashion hook

--- src/utils/hooks/useHandlePagination.ts
+++ src/utils/hooks/useHandlePagination.ts
@@ -1,12 +1,10 @@
 import { useCallback, useState } from 'react';
 
-// TODO: rename interface!!!!
 interface UseHandlePaginationCurrentPage {
   currentPage: number;
   handleCurrentPageChange: (pageNumber: number) => void;
 }
 
-//указать тип возвращаемого значения функции
 export const useHandlePagination = (): UseHandlePaginationCurrentPage => {
   const [currentPage, setCurrentPage] = useState(1);
 
