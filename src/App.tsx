be1de2cef6df23633d9b33a5de8f929135d22b4f
Vova <petrovic_h@mail.ru>
2025-03-11 13:32:06 +0300

    hot fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import './app.scss';
+import './App.scss';
 import ErrorBoundary from './components/errorBoundary';
 import { routes } from './routes';
 import './styles/globals.scss';
